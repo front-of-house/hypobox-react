957aa555ec94159f3b14fd22be05adf28ba86f94
estrattonbailey <ericstrattonbailey@gmail.com>
2021-02-08 20:37:40 -0600

    test(fix): fix broken tests

--- test/index.test.js
+++ test/index.test.js
@@ -1,28 +1,74 @@
 import React from 'react'
 import { renderToString } from 'react-dom/server'
 import { hypostyle } from 'hypostyle'
-import defaultTheme from 'hypostyle/presets/default'
+import presets from 'hypostyle/presets'
 
 import { Hypo, Box } from '../'
 
 module.exports = (test, assert) => {
-  test('base', () => {
-    const hypo = hypostyle({
-      ...defaultTheme,
-      tokens: {
-        color: {
-          primary: 'tomato'
-        },
-        ...defaultTheme.tokens
-      }
-    })
+  test('no styles', () => {
+    const hypo = hypostyle()
 
-    renderToString(
+    const html = renderToString(
       <Hypo value={hypo}>
-        <Box c='primary' />
+        <Box />
       </Hypo>
     )
-    const stylesheet = hypo.flush()
-    assert(stylesheet.includes('color:tomato'))
+
+    assert(/class=""/.test(html))
+  })
+
+  test('base, pick', () => {
+    const hypo = hypostyle(presets)
+    const html = renderToString(
+      <Hypo value={hypo}>
+        <Box o={1} style={{ background: 'blue' }} />
+      </Hypo>
+    )
+    const sheet = hypo.flush()
+
+    assert(/order:1/.test(sheet))
+    assert(/style.+background/.test(html))
+  })
+
+  test('with as', () => {
+    const hypo = hypostyle(presets)
+    const html = renderToString(
+      <Hypo value={hypo}>
+        <Box as='img' src='' />
+      </Hypo>
+    )
+
+    assert(/img.+src/.test(html))
+  })
+
+  test('className', () => {
+    const hypo = hypostyle(presets)
+    const html = renderToString(
+      <Hypo value={hypo}>
+        <Box className='foo' w={10} />
+      </Hypo>
+    )
+
+    assert(/foo\s/.test(html))
+  })
+
+  test('configure', () => {
+    const hypo = hypostyle({
+      ...presets,
+      tokens: {
+        color: {
+          b: 'blue'
+        }
+      }
+    })
+    renderToString(
+      <Hypo value={hypo}>
+        <Box className='foo' c='b' />
+      </Hypo>
+    )
+    const sheet = hypo.flush()
+
+    assert(/color:blue/.test(sheet))
   })
 }
